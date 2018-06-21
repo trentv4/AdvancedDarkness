26ebe69989107b086722b488262047f78154b15f
Trent VanSlyke <trentvanslyke@gmail.com>
2018-06-21 14:21:36 -0400

    Increment version number and add changelog

--- src/main/java/net/trentv/minecraft/darkness/AdvancedDarkness.java
+++ src/main/java/net/trentv/minecraft/darkness/AdvancedDarkness.java
@@ -17,7 +17,7 @@ public class AdvancedDarkness
 {
 	public static final String NAME = "Advanced Darkness";
 	public static final String MODID = "advanced_darkness";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.4";
 	public static final String GUI_FACTORY = "net.trentv.minecraft.darkness.AdvancedDarknessConfiguration";
 
 	public static ModConfig config;
